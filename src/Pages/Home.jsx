888cc60a3fb4c9760451e1e20dd431004108c0ef
Leïla Pelletier <leila.plltr@gmail.com>
2025-11-10 16:50:40 +0100

    Création du composant Feature + intégration à la page d'accueil

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,10 @@
 import Header from '../Components/Header.jsx'
 import Hero from '../Components/Hero.jsx'
+import Feature from '../Components/Feature.jsx'
+import image1 from '../assets/icon-chat.png'
+import image2 from '../assets/icon-money.png'
+import image3 from '../assets/icon-security.png'
+
 
 function Home() {
     return (
@@ -7,6 +12,23 @@ function Home() {
             <Header />
             <main>
                 <Hero />
+                <section className="features">
+                    <h2 className="sr-only">Features</h2>
+                    <Feature 
+                        image={image1}
+                        title="You are our #1 priority"
+                        text="Need to talk to a representative? You can get in touch through our
+            24/7 chat or through a phone call in less than 5 minutes."/>
+                    <Feature 
+                        image={image2}
+                        title="More savings means higher rates"
+                        text="The more you save with us, the higher your interest rate will be!"/>
+                    <Feature 
+                        image={image3}
+                        title="Security you can trust"
+                        text="We use top of the line encryption to make sure your data and money
+            is always safe."/>
+                </section>
             </main>
         </>
     )
