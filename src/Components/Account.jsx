c9ccc8cec23f1a467eedde3366906ec5086ee405
Leïla Pelletier <leila.plltr@gmail.com>
2025-11-10 17:55:39 +0100

    Création de la page user + composants Accounts

---
+++ src/Components/Account.jsx
@@ -0,0 +1,32 @@
+import '../css/account.css'
+
+function Account ({ title, price , balance }) {
+    return (
+        <>
+            <section className="account">
+                <div className="account-content-wrapper">
+                    <h3 className="account-title">{title}</h3>
+                    <p className="account-amount">{price}</p>
+                    <p className="account-amount-description">{balance}</p>
+                </div>
+                <div className="account-content-wrapper cta">
+                    <button className="transaction-button">View transactions</button>
+                    </div>
+            </section>
+        </>
+    )
+}
+
+export default Account;
+
+
+/*<section class="account">
+        <div class="account-content-wrapper">
+          <h3 class="account-title">Argent Bank Checking (x8349)</h3>
+          <p class="account-amount">$2,082.79</p>
+          <p class="account-amount-description">Available Balance</p>
+        </div>
+        <div class="account-content-wrapper cta">
+          <button class="transaction-button">View transactions</button>
+        </div>
+      </section>*/
--- src/Pages/User.jsx
+++ src/Pages/User.jsx
@@ -1,12 +1,30 @@
 import React from 'react'
-
+import '../css/user.css'
+import Account from '../Components/Account.jsx'
 
 function User () {
     return (
-        <div className="user">
-            <h1>User Page</h1>
-            <p>Welcome to your user dashboard.</p>
-        </div>
+        <>
+        <main className="bg-dark">
+            <div className="header">
+                <h1>Welcome back<br />Tony Jarvis!</h1>
+                <button className="edit-button">Edit Name</button>
+            </div>
+            <h2 className="sr-only">Accounts</h2>
+            <Account 
+                title="Argent Bank Checking (x8349)"
+                price="$2,082.79"
+                balance="Available Balance"/>
+            <Account 
+                title="Argent Bank Savings (x6712)"
+                price="$10,928.42"
+                balance="Available Balance"/>
+            <Account 
+                title="Argent Bank Credit Card (x8349)"
+                price="$184.30"
+                balance="Current Balance"/>
+        </main>
+        </>
     )
 }
 
