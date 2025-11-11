5f0b3ca25165ec20d2a913a8ebb32f150078b979
Leïla Pelletier <leila.plltr@gmail.com>
2025-11-11 10:09:02 +0100

    Ajout de l'en-tête de la page (modification à venir)

--- src/Pages/Transaction.jsx
+++ src/Pages/Transaction.jsx
@@ -1,11 +1,19 @@
 import Header from '../Components/Header.jsx'
 import React from 'react'
+import Account from '../Components/Account.jsx'
+import '../css/transaction.css'
 
 function Transaction() {
     return (
         <>
             <Header
             title="Tony"/>
+            <main className="bg-dark">
+                <Account 
+                    title="Argent Bank Checking (x8349)"
+                    price="$2,082.79"
+                    balance="Available Balance"/>
+            </main>
         </>
     )
 }
