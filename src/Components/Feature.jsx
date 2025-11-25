936aada6101ac32f360be0df4b4c331ed286a923
Leïla Pelletier <leila.plltr@gmail.com>
2025-11-25 13:32:35 +0100

    Correction des alt sur la partie Home

--- src/Components/Feature.jsx
+++ src/Components/Feature.jsx
@@ -1,9 +1,9 @@
 import '../css/feature.css'
 
-function Feature ( {image , title, text}) {
+function Feature ( {image , title, text, description}) {
     return (
         <div className="feature-item">
-          <img src={image} alt="Chat Icon" className="feature-icon" />
+          <img src={image} alt={description} className="feature-icon" />
           <h3 className="feature-item-title">{title}</h3>
           <p>
             {text}
